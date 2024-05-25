bb9b19d0a6d65ed1f6b457c81f52f04c74504c01
smx_nan <35058335+shenmingxing@users.noreply.github.com>
2024-05-25 18:24:40 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    reactStrictMode: true,
+    head: {
+      // 在这里添加你的 Google Search Console 的元标签
+      // 示例中使用的是 Google Search Console 提供的验证代码
+      // 可以根据你自己的需求来修改
+      meta: [{ name: "google-site-verification", content: "KudLl7nJqs29wcdvH7WJ___juLEUs9kBLBHt1Fi5Es8" }],
+    },
+  };
